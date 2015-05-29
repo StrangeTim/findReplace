425203bacbb662fcc1d489dde66d1bd90c78e387
Steve <strang3tim@gmail.com>
2015-05-29 09:55:03 -0700

    add spec to check word replacement in a phrase.

--- specs/specs.js
+++ specs/specs.js
@@ -7,4 +7,11 @@ describe('findReplace', function() {
     expect(phrase).to.equal("word");
   });
 
+  it("changes only the desired word in a phrase", function () {
+    var phrase = "only change the desired word to be changed";
+    var findWord = "change";
+    phrase = findReplace([phrase, findWord, "update"]);
+    expect(phrase).to.equal("only update the desired word to be changed");
+  });
+
 });
